46ed126056279255943d24300bba40b9b4a4b273
Kavic15 <kavatomas15@seznam.cz>
2025-04-14 10:38:26 +0200

    Delete obsolete

--- project-root/network/proxy_rotator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class ProxyManager:
-    def __init__(self, proxy_list):
-        self.proxies = proxy_list
-    
-    def get_random_proxy(self):
-        return random.choice(self.proxies)
--- project-root/network/user_agent_util.py
+++
@@ -1,8 +0,0 @@
-from fake_useragent import UserAgent
-
-class UserAgentGenerator:
-    def __init__(self):
-        self.ua = UserAgent()
-
-    def get_random_ua(self):
-        return self.ua.random
